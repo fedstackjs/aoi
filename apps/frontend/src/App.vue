d67cff8648e732fc98039b56f53f171bdb2228f4
thezzisu <thezzisu@gmail.com>
2023-07-08 22:13:04 +0800

    feat: add appbar

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld />
-    </v-main>
-  </v-app>
+  <VApp>
+    <VMain>
+      <VAppBar>
+        <VAppBarNavIcon></VAppBarNavIcon>
+        <VAppBarTitle>AoiUI</VAppBarTitle>
+      </VAppBar>
+    </VMain>
+  </VApp>
 </template>
 
-<script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
