1b1094e8657b376b030c43bf5b57998c42c342ab
thezzisu <thezzisu@gmail.com>
2023-07-20 17:27:50 +0800

    feat: refactor user menu (#1)
    Reviewed-on: https://git.zisu.dev/scoreplus/aoi/pulls/1

--- apps/frontend/src/components/app/AppBar.vue
+++ apps/frontend/src/components/app/AppBar.vue
@@ -5,38 +5,20 @@
 
     <VSpacer></VSpacer>
 
-    <VBtn color="blue-darken-1" v-if="!appState.loggedIn" to="/signin">
-      {{ t('signin') }}
-    </VBtn>
-
-    <VMenu open-on-hover v-else>
-      <template v-slot:activator="{ props }">
-        <VBtn v-bind="props">
-          {{ appState.userName }}
-        </VBtn>
-      </template>
-      <VList>
-        <VListItem v-for="(item, i) in userMenu" :key="i" v-bind="item" @click="item.action" />
-      </VList>
-    </VMenu>
+    <VToolbarItems>
+      <AppBarUserMenu v-if="appState.loggedIn" />
+      <VBtn v-else color="blue-darken-1" to="/signin">
+        {{ t('signin') }}
+      </VBtn>
+    </VToolbarItems>
   </VAppBar>
 </template>
 
 <script setup lang="ts">
 import { useAppState } from '@/stores/app'
 import { useI18n } from 'vue-i18n'
-import { computed } from 'vue'
-import { useRouter } from 'vue-router'
-import { logout as _logout } from '@/utils/http'
+import AppBarUserMenu from './AppBarUserMenu.vue'
 
-const router = useRouter()
 const { t } = useI18n()
 const appState = useAppState()
-
-const userMenu = computed(() => [{ prependIcon: 'mdi-logout', action: logout, title: t('logout') }])
-
-function logout() {
-  _logout()
-  router.replace('/')
-}
 </script>
---
+++ apps/frontend/src/components/app/AppBarUserMenu.vue
@@ -0,0 +1,46 @@
+<template>
+  <VMenu>
+    <template v-slot:activator="{ props }">
+      <VBtn v-bind="props" :loading="userInfo.isLoading.value" class="text-none">
+        <template #prepend>
+          <VAvatar>
+            <AppGravatar :email="userInfo.state.value?.email" />
+          </VAvatar>
+        </template>
+        {{ userInfo.state.value?.username }}
+      </VBtn>
+    </template>
+    <VList>
+      <VListItem v-for="(item, i) in userMenu" :key="i" v-bind="item" @click="item.action" />
+    </VList>
+  </VMenu>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { logout as _logout, http } from '@/utils/http'
+import { useAsyncState } from '@vueuse/core'
+import AppGravatar from './AppGravatar.vue'
+
+const router = useRouter()
+const { t } = useI18n()
+
+const userInfo = useAsyncState(async () => {
+  const resp = await http.get('user/profile')
+  const user = await resp.json<{
+    username: string
+    realname: string
+    email: string
+  }>()
+  return user
+}, null as never)
+
+const userMenu = computed(() => [{ prependIcon: 'mdi-logout', action: logout, title: t('logout') }])
+
+function logout() {
+  _logout()
+  router.replace('/')
+}
+</script>
