671c8219303750a2e7ef01f26128584d1fb06abb
thezzisu <thezzisu@gmail.com>
2023-08-21 11:50:04 +0800

    feat(common): solution details schema (#33)
    Reviewed-on: https://git.zisu.dev/scoreplus/aoi/pulls/33

--- libs/common/src/schemas/index.ts
+++ libs/common/src/schemas/index.ts
@@ -1,2 +1,3 @@
 export * from './problem.js'
 export * from './ranklist.js'
+export * from './solution.js'
---
+++ libs/common/src/schemas/solution.ts
@@ -0,0 +1,27 @@
+import { Static, Type } from '@sinclair/typebox'
+
+export const solutionDetailsTestSchema = Type.Object({
+  name: Type.String(),
+  score: Type.Number(),
+  status: Type.String(),
+  summary: Type.String()
+})
+
+export type SolutionDetailsTest = Static<typeof solutionDetailsTestSchema>
+
+export const solutionDetailsJobSchema = Type.Object({
+  name: Type.String(),
+  score: Type.Number(),
+  scoreScale: Type.Number(),
+  tests: Type.Array(solutionDetailsTestSchema),
+  summary: Type.String()
+})
+
+export type SolutionDetailsJob = Static<typeof solutionDetailsJobSchema>
+
+export const solutionDetailsSchema = Type.Object({
+  jobs: Type.Array(solutionDetailsJobSchema),
+  summary: Type.String()
+})
+
+export type SolutionDetails = Static<typeof solutionDetailsSchema>
