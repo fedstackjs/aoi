d7872f1f5e2cc6cdc60234114a1e1e504326fde0
thezzisu <thezzisu@gmail.com>
2023-08-22 22:19:14 +0800

    fix(frontend): change build target to esnext (#44)
    Reviewed-on: https://git.zisu.dev/scoreplus/aoi/pulls/44

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -10,6 +10,9 @@ import vueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 const gitHash = execSync('git rev-parse --short HEAD').toString().trim()
 
 export default defineConfig({
+  build: {
+    target: 'esnext'
+  },
   plugins: [
     vue({
       template: { transformAssetUrls },
