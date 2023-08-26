6ac35673a1c0d43860a9cd903860637c3876e4ce
thezzisu <thezzisu@gmail.com>
2023-08-27 01:04:59 +0800

    chore(ui): about page

--- apps/frontend/src/pages/about.vue
+++ apps/frontend/src/pages/about.vue
@@ -4,7 +4,11 @@
       <VCol>
         <VCard :title="t('about-us')">
           <VCardText>
-            <p>关于...</p>
+            <div align="center">
+              <AppLogo width="128" />
+              <q class="u-text-lg u-font-italic">The <b>AOI</b> Project</q>
+            </div>
+            <MarkdownRenderer :md="t('about-us-content')" />
           </VCardText>
         </VCard>
       </VCol>
@@ -13,6 +17,8 @@
 </template>
 
 <script setup lang="ts">
+import AppLogo from '@/components/app/AppLogo.vue'
+import MarkdownRenderer from '@/components/utils/MarkdownRenderer.vue'
 import { withTitle } from '@/utils/title'
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
@@ -24,7 +30,19 @@ withTitle(computed(() => t('pages.about')))
 
 <i18n>
 en:
-  about-us: About us
+  about-us: About This Project
+  about-us-content: |
+    The AOI Project. \
+    [GitHub Repository](https://github.com/zhzxdev/aoi) \
+    [Issues & Bug Report](https://github.com/zhzxdev/aoi/issues/new) \
+    [Wiki](https://github.com/zhzxdev/aoi/wiki) \
+    [Discussions](https://github.com/zhzxdev/aoi/discussions)
 zh-Hans:
-  about-us: 关于我们
+  about-us: 关于本项目
+  about-us-content: |
+    欢迎使用AOI项目. \
+    [GitHub仓库](https://github.com/zhzxdev/aoi) \
+    [问题反馈](https://github.com/zhzxdev/aoi/issues/new) \
+    [文档](https://github.com/zhzxdev/aoi/wiki) \
+    [讨论](https://github.com/zhzxdev/aoi/discussions)
 </i18n>
