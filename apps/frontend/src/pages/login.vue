7f92374d815256dd3ce491b5563f62a568bbef77
thezzisu <thezzisu@gmail.com>
2024-01-18 14:00:15 +0800

    chore: add login hint

--- apps/frontend/src/pages/login.vue
+++ apps/frontend/src/pages/login.vue
@@ -7,6 +7,7 @@
             {{ t('pages.signin') }}
           </VCardTitle>
           <VDivider />
+          <VAlert v-if="hint" type="info" :text="hint" class="ma-4 mb-0" />
           <RouterView />
         </VCard>
       </VCol>
@@ -17,11 +18,13 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 import { useAppState } from '@/stores/app'
-import { useRouter, useRoute } from 'vue-router'
+import { useRouter } from 'vue-router'
 
 const { t } = useI18n()
 const router = useRouter()
 const appState = useAppState()
 
 if (appState.loggedIn) router.replace('/')
+
+const hint = import.meta.env.VITE_LOGIN_HINT
 </script>
