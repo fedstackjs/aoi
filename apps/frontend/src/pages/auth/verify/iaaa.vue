c6284848c71887277f21e3cf2b56b356a2fd693c
thezzisu <thezzisu@gmail.com>
2024-01-21 13:25:23 +0800

    fix: iaaa verification

--- apps/frontend/src/pages/auth/verify/iaaa.vue
+++ apps/frontend/src/pages/auth/verify/iaaa.vue
@@ -1,48 +1,16 @@
 <template>
-  <VCardText class="text-center">
-    <VProgressCircular indeterminate />
-    <p>{{ t('iaaa-wait') }}</p>
-  </VCardText>
+  <VAlert type="error" :text="t('iaaa-not-supported')" />
 </template>
 
 <script setup lang="ts">
-import { useAsyncTask } from '@/utils/async'
-import { http, login, prettyHTTPError } from '@/utils/http'
-import { onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
-import { useRouter } from 'vue-router'
-import { getToken } from '@/utils/user/iaaa'
 
 const { t } = useI18n()
-const router = useRouter()
-
-const task = useAsyncTask(async () => {
-  try {
-    const resp = await http.post('auth/login', {
-      json: {
-        provider: 'iaaa',
-        payload: {
-          token: await getToken()
-        }
-      }
-    })
-    const { token } = await resp.json<{ token: string }>()
-    login(token)
-    router.replace('/')
-  } catch (err) {
-    router.replace('/login')
-    throw new Error(await prettyHTTPError(err))
-  }
-})
-
-onMounted(() => {
-  task.execute()
-})
 </script>
 
 <i18n>
 en:
-  iaaa-wait: Waiting for IAAA login...
+  iaaa-not-supported: IAAA is not supported for verification.
 zh-Hans:
-  iaaa-wait: 等待 IAAA 登录...
+  iaaa-not-supported: IAAA 不支持验证。
 </i18n>
