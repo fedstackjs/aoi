f8820a9008b0b2c65cab309ec9422570726aa70f
thezzisu <thezzisu@gmail.com>
2024-01-21 15:09:48 +0800

    feat: allow ranklist manual update runner

--- apps/frontend/src/pages/org/[orgId]/contest/[contestId]/admin/index.vue
+++ apps/frontend/src/pages/org/[orgId]/contest/[contestId]/admin/index.vue
@@ -19,14 +19,20 @@
       >
         {{ t('action.submit-all') }}
       </VBtn>
+
       <VBtn
         color="red"
         variant="elevated"
         @click="updateRanklistsTask.execute()"
         :loading="updateRanklistsTask.isLoading.value"
+        :append-icon="resetRunner ? 'mdi-refresh' : 'mdi-pin'"
       >
         {{ t('action.update-ranklists') }}
       </VBtn>
+      <VBtn color="red" variant="outlined" @click="resetRunner = !resetRunner">
+        {{ t('reset-runner') }}
+      </VBtn>
+
       <VBtn color="red" variant="elevated" @click="deleteContest()">
         {{ t('action.delete') }}
       </VBtn>
@@ -58,6 +64,7 @@ import AccessLevelEditor from '@/components/utils/AccessLevelEditor.vue'
 import { useAsyncTask } from '@/utils/async'
 import { http } from '@/utils/http'
 import { useAsyncState } from '@vueuse/core'
+import { ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useRouter } from 'vue-router'
 
@@ -97,8 +104,14 @@ const ranklistInfo = useAsyncState(
   { immediate: true }
 )
 
+const resetRunner = ref(false)
+
 const updateRanklistsTask = useAsyncTask(() =>
-  http.post(`contest/${props.contestId}/admin/update-ranklists`)
+  http
+    .post(`contest/${props.contestId}/admin/update-ranklists`, {
+      json: { resetRunner: resetRunner.value }
+    })
+    .then(() => ranklistInfo.execute())
 )
 
 async function deleteContest() {
@@ -111,11 +124,13 @@ async function deleteContest() {
 <i18n>
 en:
   ranklist-state: Ranklist State is {state}
+  reset-runner: Switch Reset Runner
   action:
     submit-all: Submit all solutions
     update-ranklists: Update ranklists
 zh-Hans:
   ranklist-state: '排行榜状态: {state}'
+  reset-runner: 切换重置运行器
   action:
     submit-all: 提交所有解答
     update-ranklists: 更新排行榜
